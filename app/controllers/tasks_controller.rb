b7774b90e694ba06f11e8c0495738e7f8d5a9a63
ushijima eisaku <eisakuushijima@gmail.com>
2022-07-24 15:46:25 +0900

    Complete P114

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -11,4 +13,17 @@ class TasksController < ApplicationController
 
   def edit
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
