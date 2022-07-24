00c9e871db0649a6474a9977d2ced9a5c415f2e3
ushijima eisaku <eisakuushijima@gmail.com>
2022-07-24 17:12:17 +0900

    Complete chapter3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,13 +11,26 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def edit
-  end
-
   def create
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   private
