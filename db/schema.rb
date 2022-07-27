821477d7352fef1ed73bdf57ebba775ff10cdb5c
ushijima eisaku <eisakuushijima@gmail.com>
2022-07-27 23:55:47 +0900

    0727

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_23_105014) do
+ActiveRecord::Schema.define(version: 2022_07_26_113133) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
