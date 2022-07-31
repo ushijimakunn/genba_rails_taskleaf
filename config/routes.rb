1422ac68579b770d97568e8b63c2b7d9784a7bcf
ushijima eisaku <eisakuushijima@gmail.com>
2022-07-31 17:15:30 +0900

    Finish P161

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+  
   # get 'tasks/index'
   # get 'tasks/show'
   # get 'tasks/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_234224) do
+ActiveRecord::Schema.define(version: 2022_07_31_051619) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_07_27_234224) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
